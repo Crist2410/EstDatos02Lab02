602ed92b13bbea6d82027f1f2e6568592f320ea5
Crist2410 <60222691+Crist2410@users.noreply.github.com>
2020-09-13 08:56:09 -0600

    Add files via upload

---
+++ PruebasConsola/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace PruebasConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
